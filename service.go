4b554d5cabefdc875e268e77686b67dd0f7a102b
Tamas Molnar <tamas.molnar@ft.com>
2016-12-12 10:55:27 +0200

    Extracted ConceptFinder interface

--- service.go
+++ service.go
@@ -10,7 +10,11 @@ import (
 	"gopkg.in/olivere/elastic.v3"
 )
 
-type esSearcherService struct {
+type conceptFinder interface {
+	findConcept(writer http.ResponseWriter, request *http.Request)
+}
+
+type esConceptFinder struct {
 	elasticClient     *elastic.Client
 	indexName         string
 	searchResultLimit int
@@ -23,19 +27,16 @@ type esAccessConfig struct {
 	esRegion   string
 }
 
-func newESSearcherService(accessConfig *esAccessConfig, indexName string, searchResultLimit int) (*esSearcherService, error) {
+func newEsConceptFinder(accessConfig *esAccessConfig, indexName string, searchResultLimit int) (*esConceptFinder, error) {
 	elasticClient, err := newElasticClient(accessConfig.accessKey, accessConfig.secretKey, &accessConfig.esEndpoint, &accessConfig.esRegion)
 	if err != nil {
-		return &esSearcherService{}, fmt.Errorf("creating elasticsearch client failed with error=[%v]", err)
+		return &esConceptFinder{}, fmt.Errorf("creating elasticsearch client failed with error=[%v]", err)
 	}
-
-	elasticSearcher := esSearcherService{elasticClient: elasticClient, indexName: indexName, searchResultLimit: searchResultLimit}
-
+	elasticSearcher := esConceptFinder{elasticClient: elasticClient, indexName: indexName, searchResultLimit: searchResultLimit}
 	return &elasticSearcher, nil
 }
 
-func (service *esSearcherService) SearchConcept(writer http.ResponseWriter, request *http.Request) {
-
+func (service *esConceptFinder) FindConcept(writer http.ResponseWriter, request *http.Request) {
 	if service.elasticClient == nil {
 		log.Errorf("Elasticsearch client is not created.")
 		writer.WriteHeader(http.StatusInternalServerError)
@@ -69,30 +70,33 @@ func (service *esSearcherService) SearchConcept(writer http.ResponseWriter, requ
 
 	if searchResult.Hits.TotalHits > 0 {
 		writer.Header().Add("Content-Type", "application/json")
-		searchedConcepts := getSearchedConcepts(searchResult, isScoreIncluded(request))
+		foundConcepts := getFoundConcepts(searchResult, isScoreIncluded(request))
 		encoder := json.NewEncoder(writer)
-		encoder.Encode(&searchedConcepts)
+		if err := encoder.Encode(&foundConcepts); err != nil {
+			log.Errorf("Cannot encode result: %s", err.Error())
+			writer.WriteHeader(http.StatusInternalServerError)
+		}
 	} else {
 		writer.WriteHeader(http.StatusNotFound)
 	}
 }
 
-func getSearchedConcepts(searchResult *elastic.SearchResult, isScoreIncluded bool) []concept {
-	var searchedConcepts []concept
+func getFoundConcepts(searchResult *elastic.SearchResult, isScoreIncluded bool) []concept {
+	var foundConcepts []concept
 	for _, hit := range searchResult.Hits.Hits {
-		var searchedConcept concept
-		err := json.Unmarshal(*hit.Source, &searchedConcept)
+		var foundConcept concept
+		err := json.Unmarshal(*hit.Source, &foundConcept)
 		if err != nil {
 			log.Errorf("Unable to unmarshall concept, error=[%s]\n", err)
 		} else {
 			if isScoreIncluded {
 				score := *hit.Score
-				searchedConcept.Score = score
+				foundConcept.Score = score
 			}
-			searchedConcepts = append(searchedConcepts, searchedConcept)
+			foundConcepts = append(foundConcepts, foundConcept)
 		}
 	}
-	return searchedConcepts
+	return foundConcepts
 }
 
 func isScoreIncluded(request *http.Request) bool {
