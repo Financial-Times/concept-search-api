3a76bd6a6e3f31429f9b574ae36b8fdeb4aed8bd
Dragos Oana <dragos.oana@ft.com>
2016-12-22 16:51:16 +0200

    Improved the ES query to have higher score for exact matches

--- service.go
+++ service.go
@@ -57,7 +57,9 @@ func (service esConceptFinder) FindConcept(writer http.ResponseWriter, request *
 	transactionID := transactionidutils.GetTransactionIDFromRequest(request)
 	log.Infof("Performing concept search for term=%v, transaction_id=%v", *criteria.Term, transactionID)
 
-	query := elastic.NewMultiMatchQuery(criteria.Term, "prefLabel.raw", "aliases.raw", "prefLabel", "aliases").Type("most_fields")
+	query := conceptQuery{
+		Term: *criteria.Term,
+	}
 
 	searchResult, err := service.client.query(service.indexName, query, service.searchResultLimit)
 
