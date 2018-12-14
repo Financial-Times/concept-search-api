fb226d7eb1365e46a3bd7bb34e58fa5a14970e54
Pete Clark <peter.clark@ft.com>
2018-12-14 11:51:50 +0000

    :white_check_mark: Adding a test case

--- service/es_client.go
+++ service/es_client.go
@@ -41,12 +41,12 @@ func cloneRequest(r *http.Request) *http.Request {
 	// shallow copy of the struct
 	r2 := new(http.Request)
 	*r2 = *r
+	r2.Host = r.Host
 	// deep copy of the Header
 	r2.Header = make(http.Header, len(r.Header))
 	for k, s := range r.Header {
 		r2.Header[k] = append([]string(nil), s...)
 	}
-	r2.Host = r.Host
 	return r2
 }
 
